de63622d7b18e6e68baf10e4094097af6f7af096
willizambrano <zambranowilliam21@gmail.com>
2020-10-03 04:08:41 -0400

    Revert all simple_msm_thermal commits

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -776,7 +776,9 @@ static ssize_t store_##file_name					\
 	int ret, temp;							\
 	struct cpufreq_policy new_policy;				\
 									\
-	return count;							\
+	if (&policy->object == &policy->min                             \
+	    || disable_userspace_boost )				\
+		return count;						\
 									\
 	memcpy(&new_policy, policy, sizeof(*policy));			\
 	new_policy.min = policy->user_policy.min;			\
