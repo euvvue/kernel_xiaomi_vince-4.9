052b563b0670047f752722dd000fb4500367a6e7
willizambrano <zambranowilliam21@gmail.com>
2020-10-03 16:12:45 -0400

    [PATCH] vidc_3x: Flush delayed pm work in suspend
    During device suspend, venus powercollapse is to done
    immediately without timer, to save power.
    
    Change-Id: I676b709c6585378ecc49202250dac2f06b44a949
    Signed-off-by: Vasantha Balla <vballa@codeaurora.org>

--- drivers/media/platform/msm/vidc_3x/venus_hfi.c
+++ drivers/media/platform/msm/vidc_3x/venus_hfi.c
@@ -1337,6 +1337,9 @@ static int venus_hfi_suspend(void *dev)
 		return -ENOTSUPP;
 	}
 
+    dprintk(VIDC_DBG, "Suspending Venus\n");
+	flush_delayed_work(&venus_hfi_pm_work);
+
 	mutex_lock(&device->lock);
 
 	if (device->power_enabled) {
